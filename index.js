bfb35e3624d1a0f99e8d3cd170489a2d7827e902
ejyang <yangej0218@gmail.com>
2024-09-06 22:58:52 +0800

    feat: make promise chainable

--- index.js
+++ index.js
@@ -1,42 +1,81 @@
 const STATE = {
-    PENDING: 'pending',
-    FULFILLED: 'fulfilled',
-    REJECTED: 'rejected',
-}
+  PENDING: "pending",
+  FULFILLED: "fulfilled",
+  REJECTED: "rejected",
+};
 
 class MyPromise {
-    #state = STATE.PENDING;
-    #value;
-    #resolveCallbacks = [];
-    #rejectCallbacks = [];
+  #state = STATE.PENDING;
+  #value;
+  #resolveCallbacks = [];
+  #rejectCallbacks = [];
+  #boundResolve = this.#resolve.bind(this);
+  #boundReject = this.#reject.bind(this);
 
   constructor(callback) {
     try {
-      callback(this.#resolve, this.#reject);
+      callback(this.#boundResolve, this.#boundReject);
     } catch (error) {
       this.#reject(error);
     }
   }
 
   #resolve(value) {
-    if (this.#state !== STATE.PENDING) return;
+    // queue the task, execute after the sync method `then` is called
+    queueMicrotask(() => {
+      if (this.#state !== STATE.PENDING) return;
 
-    this.#value = value;
-    this.#state = STATE.FULFILLED;
-    this.#resolveCallbacks.forEach((callback) => callback(this.#value));
+      this.#value = value;
+      this.#state = STATE.FULFILLED;
+      this.#resolveCallbacks.forEach((callback) => {
+        callback(this.#value);
+      });
+    });
   }
 
   #reject(value) {
-    if (this.#state !== STATE.PENDING) return;
-    
-    this.#value = value;
-    this.#state = STATE.REJECTED;
-    this.#rejectCallbacks.forEach((callback) => callback(this.#value));
+    // queue the task, execute after the sync method `then` is called
+    queueMicrotask(() => {
+      if (this.#state !== STATE.PENDING) return;
+
+      this.#value = value;
+      this.#state = STATE.REJECTED;
+      this.#rejectCallbacks.forEach((callback) => callback(this.#value));
+    });
   }
 
   then(resolveCallback, rejectCallback) {
-    this.#resolveCallbacks.push((value) => resolveCallback(value));
-    this.#rejectCallbacks.push((value) => rejectCallback(value));
+    return new MyPromise((resolve, reject) => {
+      this.#resolveCallbacks.push((value) => {
+        if (resolveCallback == null) {
+          resolve(value);
+
+          return;
+        }
+
+        try {
+          const result = resolveCallback(value);
+          resolve(result);
+        } catch (error) {
+          reject(error);
+        }
+      });
+
+      this.#rejectCallbacks.push((value) => {
+        if (rejectCallback == null) {
+          reject(value);
+
+          return;
+        }
+
+        try {
+          const result = rejectCallback(value);
+          resolve(result);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    });
   }
 }
 
